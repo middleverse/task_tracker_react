94029efa3d62e90d7b748a35986cae2555236221
Arshi Bhasin <arshi_b@hotmail.com>
2021-02-18 12:40:56 -0500

    form component UI toggle

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddTask from './components/AddTask'
 
 const App = () => {
 
+  const [showAddTask, setShowAddTask] = useState(false)
   const [ tasks, setTasks ] = useState ([
     {
         id: 1,
@@ -44,8 +45,9 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {/* ternary operator below without an else, if showAddTask, show */}
+      { showAddTask && <AddTask onAdd={addTask} /> } 
       { tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
         ) : (
