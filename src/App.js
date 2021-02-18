b430b12f77e1b8db38e428f5faee0696dbdbcef1
Arshi Bhasin <arshi_b@hotmail.com>
2021-02-18 08:41:50 -0500

    added delete task functionality

--- src/App.js
+++ src/App.js
@@ -20,10 +20,19 @@ const App = () => {
     },
   ])
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      { tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+        ) : (
+          'No tasks to show'
+        )
+      }
     </div>
   );
 }
