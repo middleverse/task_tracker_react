1d890267bf4c9d2bb7ece1c7dbfafa9e72d8d1af
Arshi Bhasin <arshi_b@hotmail.com>
2021-02-18 08:28:41 -0500

    added icons to tasks, as well as new Task component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
-
-
 const App = () => {
 
   const [ tasks, setTasks ] = useState ([
